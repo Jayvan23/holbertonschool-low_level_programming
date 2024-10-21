4491be4aeaa6cc14ea1faa04c8b6e54f047c69d9
Jayvan <jayvanramos23@gmail.com>
2024-10-21 15:10:29 -0400

    fixing 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#/usr/bin/env gcc -o puzzle -x c -
-#include <stdio.h> puts("Programming is like building a multilingual puzzle"); return 0;
+#include <stdio.h>
+
+int main(void) {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
