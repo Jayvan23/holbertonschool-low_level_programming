4cd5828118c5ae42e0177870cb25dda8b90139c5
Jayvan <jayvanramos23@gmail.com>
2024-10-21 16:35:22 -0400

    fixing 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a specific string
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
