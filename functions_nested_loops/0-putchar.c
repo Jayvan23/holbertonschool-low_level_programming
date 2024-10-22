66c55b86ec87afe4e33ec946a3eca8999f7def41
Jayvan <jayvanramos23@gmail.com>
2024-10-22 13:31:14 -0400

    added 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,17 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-return (0);
+    printf("_putchar\n");
+    return (0);
 }
