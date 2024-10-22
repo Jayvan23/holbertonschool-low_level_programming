534359b0c43eb15c64f13c1deaa640ce54a9fcdf
Jayvan <jayvanramos23@gmail.com>
2024-10-22 13:35:06 -0400

    fixing  0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    printf("_putchar\n");
-    return (0);
+write(1, "_putchar\n", 9);
+return (0);
 }
