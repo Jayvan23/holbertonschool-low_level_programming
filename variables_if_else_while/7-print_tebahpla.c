5ccc3d4040c76a8287e0a05225c6a1f9ea796f4d
Jayvan <jayvanramos23@gmail.com>
2024-10-22 10:37:45 -0400

    added 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
