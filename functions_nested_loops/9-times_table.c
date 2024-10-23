f389fcd39791ec55f97ff6cf64025b0309738e1a
Jayvan <jayvanramos23@gmail.com>
2024-10-23 14:52:41 -0400

    fixing 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,20 +7,19 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 {
-	for (j = 0; j <10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		product = i * j;
 
 		if (product < 10)
 		{
 			_putchar('0' + product);
-			/*_putchar(' ');*/
+			_putchar(' ');
 		}
 		else
 		{
 			_putchar('0' + (product / 10));
 			_putchar('0' + (product % 10));
-			_putchar(' ');
 		}
 
 		if (j < 9)
