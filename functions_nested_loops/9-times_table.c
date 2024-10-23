0a5acef9fa1adda1eb44b9030355fa57fd2308b2
Jayvan <jayvanramos23@gmail.com>
2024-10-23 13:41:36 -0400

    fixing 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,34 +1,32 @@
 #include <unistd.h>
-
-/**
- * times_table - Prints the 9 times table, starting with 0.
- *
- * This function prints the multiplication table for the numbers
- * 0 through 9, formatted in a grid with each number separated by
- * a comma and a space.
- */
+#include "main.h"
 
 void times_table(void)
 {
-    int i, j;
+	int i, j, product;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-	{
-            int product = i * j;
-            print_number(product);
-            if (j < 9) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
-}
-
-int main(void)
+	for (i = 0; i < 10; i++)
 {
-    times_table();
-    return (0);
+	for (j = 0; j <10; j++)
+	{
+		product = i * j;
+
+		if (product < 10)
+		{
+			_putchar('0' + product);
+		}
+		else
+		{
+			_putchar('0' + (product / 10));
+			_putchar('0' + (product % 10));
+		}
+
+		if (j < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
 }
