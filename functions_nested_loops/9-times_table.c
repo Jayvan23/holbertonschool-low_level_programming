27418e8cdecf3f7753c43b2e7df03d950501e005
Jayvan <jayvanramos23@gmail.com>
2024-10-23 15:30:37 -0400

    fixing 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,40 +1,47 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * times_table - prints the nine times table, starting with cero
+ *
+ *
+ * Return: void
+ */
+
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, product;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            product = i * j;
-
-            if (product < 10)
-            {
-                if (j != 0)
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
 		{
-                    _putchar(' ');
-                }
-                _putchar('0' + product);
-                if (j < 9)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
-            }
-            else
-            {
-                _putchar('0' + (product / 10));
-                _putchar('0' + (product % 10));
-                if (j < 9)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
-            }
-        }
-        _putchar('\n');
-    }
+			product = i * j;
+
+			if (product < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar('0' + product);
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
