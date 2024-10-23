d63b38ae57bba90646f16b89c028f0f28ee837d2
Jayvan <jayvanramos23@gmail.com>
2024-10-23 11:17:50 -0400

    fixing 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * jack_bauer - Prints every minute of the day from 00:00 to 23:59.
@@ -8,12 +8,20 @@
 void jack_bauer(void)
 {
 int hour, minute;
+char buffer[6];
 
 for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
-printf("%02d:%02d\n", hour, minute);
+buffer[0] = (hour / 10) + '0';
+buffer[1] = (hour % 10) + '0';
+buffer[2] = ':';
+buffer[3] = (minute / 10) + '0';
+buffer[4] = (minute % 10) + '0';
+buffer[5] = '\n';
+            
+write(1, buffer, 6);
 }
 }
 }
