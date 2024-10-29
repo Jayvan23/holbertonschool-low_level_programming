83732056a4400e48ee9757a04168743755e40e61
Jayvan <jayvanramos23@gmail.com>
2024-10-29 12:31:42 -0400

    added 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: The first string to compare
  * @s2: The second string to compare
  *
- * Return: Negative value if s1 < s2, positive if s1 > s2, or 0 if they are equal
+ * Return: Negative value if s1 < s2, positive if s1 > s2, or 0 if equal
  */
 
 int _strcmp(char *s1, char *s2)
