6e8f0c958b282ec9860b5bdb6b91377afcd80c59
Jayvan <jayvanramos23@gmail.com>
2024-10-29 10:45:11 -0400

    added 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 
+/**
+ * puts_half - Prints the second half of a string.
+ * @str: Pointer to the string to be printed.
+ */
+
 void puts_half(char *str)
 {
 	int length = 0;
@@ -19,7 +24,7 @@ void puts_half(char *str)
 	{
 		start = str + ((length - 1) / 2 + 1);
 	}
-	
+
 	while (*start)
 	{
 		write(1, start, 1);
