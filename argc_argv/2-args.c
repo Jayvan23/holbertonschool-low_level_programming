9ef35b77fa38fba53a04d5de40b44260c5b5bbd5
Jayvan <jayvanramos23@gmail.com>
2024-11-06 12:26:01 -0400

    fixed 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -3,15 +3,22 @@
 /**
  * main - Prints all arguments it receives, one per line.
  * @argc: The number of command-line arguments.
- * @argv: The array of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
  *
- * Return: Always 0 (Success)
+ * Description:
+ * This program prints each argument received, including the program's name,
+ * followed by a new line. It prints one argument per line.
+ *
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	for (int i = 0; i < argc; i++)
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
+
 	return (0);
 }
