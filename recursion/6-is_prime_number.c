9746aad0fc5a1511fccd583adb8b7527d8e844a7
Jayvan <jayvanramos23@gmail.com>
2024-11-06 11:12:47 -0400

    fixing 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,26 +1,34 @@
 #include "main.h"
-#include <math.h>
 
 /**
- * is_prime_number - checks if a number is prime
- * @n: the number to check
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to check.
  *
- * Return: 1 if the number is prime, otherwise 0
+ * Return: 1 if n is prime, otherwise 0.
+ *
+ * Description: This function checks if the input number `n` is a prime
+ * number. A prime number is a number greater than 1 that is only divisible
+ * by 1 and itself. The function returns 1 if the number is prime and 0
+ * otherwise. The check is performed by testing divisibility up to the
+ * square root of `n` for odd numbers greater than 2. Even numbers greater
+ * than 2 are automatically considered not prime.
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	if (n == 2)
-		return (1);
-	if (n % 2 == 0)
-		return (0);
+    int i;
 
-	for (int i = 3; i * i <= n; i += 2)
-	{
-		if (n % i == 0)
-			return (0);
-	}
+    if (n <= 1)
+        return (0);
+    if (n == 2)
+        return (1);
+    if (n % 2 == 0)
+        return (0);
 
-	return (1);
+    for (i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0)
+            return (0);
+    }
+
+    return (1);
 }
