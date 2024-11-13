9ac87b6ae44be2b0fcda48555ad9ea2ea51ed017
Jayvan <jayvanramos23@gmail.com>
2024-11-13 11:11:47 -0400

    added 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure representing a dog
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
