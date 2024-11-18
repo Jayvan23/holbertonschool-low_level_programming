0aacf78eade5855d193af593976bdf7ff9ebb99b
Jayvan <jayvanramos23@gmail.com>
2024-11-18 14:14:19 -0400

    fixing 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,40 +1,52 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
-* main - Prints calculator result
-*
-* @argc: int of commandline args
-*
-* @argv: 2-d array of commandline args
-*
-* Return: 0 success, 98 for argc error, 100 if div 0
-*/
-
-int main(int argc, char **argv)
+ * main - Performs simple operations based on user input.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings representing the arguments.
+ * 
+ * Return: 0 on success.
+ *         Exits with status 98 if the number of arguments is incorrect.
+ *         Exits with status 99 if the operator is invalid or not a single character.
+ *         Exits with status 100 if there is an attempt to divide or modulo by zero.
+ */
+int main(int argc, char *argv[])
 {
-	int (*operator_function)(int, int), num1, num2;
+    int num1, num2;
+    int (*operation)(int, int);
 
-	if (argc != 4)
-		printf("Error\n"), exit(98);
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-	operator_function = get_op_func(argv[2]);
-	if (!operator_function)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    if (argv[2][1] != '\0')
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+    operation = get_op_func(argv[2]);
 
-	printf("%d\n", operator_function(num1, num2));
-	return (0);
+    if (operation == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    printf("%d\n", operation(num1, num2));
+
+    return (0);
 }
