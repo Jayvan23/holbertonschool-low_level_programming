fed4dbe5e3e7dad2267360d4f25cca4763dc660d
Jayvan <jayvanramos23@gmail.com>
2024-11-18 14:02:11 -0400

    fixing 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -18,7 +18,7 @@ int (*get_op_func(char *s))(int, int)
     int i = 0;
 
     if (s[1] != '\0')
-        return NULL;
+        return (NULL);
 
     while (ops[i].op != NULL)
     {
@@ -27,5 +27,5 @@ int (*get_op_func(char *s))(int, int)
         i++;
     }
 
-    return NULL;
+    return (NULL);
 }
