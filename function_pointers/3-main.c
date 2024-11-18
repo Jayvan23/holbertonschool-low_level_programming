94955770a259266e2fb838889dba13b8bec0098c
Jayvan <jayvanramos23@gmail.com>
2024-11-18 14:15:59 -0400

    fixing 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -17,28 +17,14 @@ int main(int argc, char *argv[])
     int num1, num2;
     int (*operation)(int, int);
 
-    if (argc != 4)
+    if (argc != 4 || argv[2][1] != '\0' || (operation = get_op_func(argv[2])) == NULL)
     {
         printf("Error\n");
-        exit(98);
+        exit(argc != 4 ? 98 : 99);
     }
 
     num1 = atoi(argv[1]);
     num2 = atoi(argv[3]);
-
-    if (argv[2][1] != '\0')
-    {
-        printf("Error\n");
-        exit(99);
-    }
-
-    operation = get_op_func(argv[2]);
-
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
 
     if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
     {
