02c25f2e027a6249ff004c01eca5c91d230ff21d
Jayvan <jayvanramos23@gmail.com>
2025-02-15 17:11:30 -0400

    fixing codes

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0.
- * @n: The number to print the times table for.
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number for which the times table is printed.
  *
- * Return: Nothing. If n is less than 0 or greater than 15, do nothing.
+ * Description: If n is greater than 15 or less than 0, the function does nothing.
+ *              The table is formatted with proper spacing.
  */
 void print_times_table(int n)
 {
     int i, j, result;
 
     if (n < 0 || n > 15)
-    {
         return;
-    }
 
     for (i = 0; i <= n; i++)
     {
@@ -21,31 +20,34 @@ void print_times_table(int n)
         {
             result = i * j;
 
-            if (j != 0)
+            if (j == 0)
+                _putchar('0');
+            else
             {
                 _putchar(',');
                 _putchar(' ');
+
+                if (result < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                else if (result < 100)
+                {
+                    _putchar(' ');
+                }
             }
 
-            if (result < 10)
+            if (result >= 100)
             {
-                if (j != 0)
-                    _putchar(' ');
-                _putchar(' ');
-                _putchar('0' + result);
+                _putchar((result / 100) + '0');
+                _putchar(((result / 10) % 10) + '0');
             }
-            else if (result < 100)
+            else if (result >= 10)
             {
-                _putchar(' ');
-                _putchar('0' + result / 10);
-                _putchar('0' + result % 10);
+                _putchar((result / 10) + '0');
             }
-            else
-            {
-                _putchar('0' + result / 100);
-                _putchar('0' + (result / 10) % 10);
-                _putchar('0' + result % 10);
-            }
+            _putchar((result % 10) + '0');
         }
         _putchar('\n');
     }
