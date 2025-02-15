ab4e6df92fd3361d4c86aa13042f2de0f6d505e6
Jayvan <jayvanramos23@gmail.com>
2025-02-15 17:21:54 -0400

    fixing codes

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,18 +8,29 @@
 int main(void)
 {
     int count;
-    unsigned long fib1 = 1, fib2 = 2, next;
+    unsigned long fib1_low = 1, fib1_high = 0;
+    unsigned long fib2_low = 2, fib2_high = 0;
+    unsigned long next_low, next_high;
+    unsigned long carry;
 
-    printf("%lu, %lu", fib1, fib2);
+    printf("%lu, %lu", fib1_low, fib2_low);
 
     for (count = 2; count < 98; count++)
     {
-        next = fib1 + fib2;
-        printf(", %lu", next);
+        next_low = fib1_low + fib2_low;
+        carry = next_low / 1000000000;
+        next_low %= 1000000000;
+        next_high = fib1_high + fib2_high + carry;
 
-        /* Update fib1 and fib2 for the next iteration */
-        fib1 = fib2;
-        fib2 = next;
+        if (next_high == 0)
+            printf(", %lu", next_low);
+        else
+            printf(", %lu%09lu", next_high, next_low);
+
+        fib1_low = fib2_low;
+        fib1_high = fib2_high;
+        fib2_low = next_low;
+        fib2_high = next_high;
     }
 
     printf("\n");
