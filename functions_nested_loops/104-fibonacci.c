57307586051047013a946dff7648b7c3164f03c8
Jayvan <jayvanramos23@gmail.com>
2025-02-15 17:24:06 -0400

    fixing codes

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,32 +7,32 @@
  */
 int main(void)
 {
-    int count;
-    unsigned long fib1_low = 1, fib1_high = 0;
-    unsigned long fib2_low = 2, fib2_high = 0;
-    unsigned long next_low, next_high;
-    unsigned long carry;
+	int count;
+	unsigned long fib1_low = 1, fib1_high = 0;
+	unsigned long fib2_low = 2, fib2_high = 0;
+	unsigned long next_low, next_high;
+	unsigned long carry;
 
-    printf("%lu, %lu", fib1_low, fib2_low);
+	printf("%lu, %lu", fib1_low, fib2_low);
 
-    for (count = 2; count < 98; count++)
-    {
-        next_low = fib1_low + fib2_low;
-        carry = next_low / 1000000000;
-        next_low %= 1000000000;
-        next_high = fib1_high + fib2_high + carry;
+	for (count = 2; count < 98; count++)
+	{
+		next_low = fib1_low + fib2_low;
+		carry = next_low / 1000000000;
+		next_low %= 1000000000;
+		next_high = fib1_high + fib2_high + carry;
 
-        if (next_high == 0)
-            printf(", %lu", next_low);
-        else
-            printf(", %lu%09lu", next_high, next_low);
+		if (next_high == 0)
+			printf(", %lu", next_low);
+		else
+			printf(", %lu%09lu", next_high, next_low);
 
-        fib1_low = fib2_low;
-        fib1_high = fib2_high;
-        fib2_low = next_low;
-        fib2_high = next_high;
-    }
+		fib1_low = fib2_low;
+		fib1_high = fib2_high;
+		fib2_low = next_low;
+		fib2_high = next_high;
+	}
 
-    printf("\n");
-    return (0);
+	printf("\n");
+	return (0);
 }
