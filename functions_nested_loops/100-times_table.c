4ff29215f54aca09c5cead1852611db7db9513d2
Jayvan <jayvanramos23@gmail.com>
2025-02-15 16:53:46 -0400

    fixing codes

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -33,10 +33,12 @@ void print_times_table(int n)
                 if (result < 10)
                 {
                     _putchar(' ');
+                    _putchar(' ');
                     _putchar('0' + result);
                 }
                 else if (result < 100)
                 {
+                    _putchar(' ');
                     _putchar('0' + result / 10);
                     _putchar('0' + result % 10);
                 }
