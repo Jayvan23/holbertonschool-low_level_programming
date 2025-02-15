8a9dea04aa2e281bf954990b7c7edfc04b9bd433
Jayvan <jayvanramos23@gmail.com>
2025-02-15 17:08:01 -0400

    fixing codes

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,40 +1,6 @@
 #include "main.h"
 
 /**
- * print_single_number - prints a single number with proper formatting
- * @result: The result to print.
- * @is_first: Boolean indicating if it is the first number in the row.
- */
-void print_single_number(int result, int is_first)
-{
-    if (!is_first)
-    {
-        _putchar(',');
-        _putchar(' ');
-    }
-
-    if (result < 10)
-    {
-        if (!is_first)
-            _putchar(' ');
-        _putchar(' ');
-        _putchar('0' + result);
-    }
-    else if (result < 100)
-    {
-        _putchar(' ');
-        _putchar('0' + result / 10);
-        _putchar('0' + result % 10);
-    }
-    else
-    {
-        _putchar('0' + result / 100);
-        _putchar('0' + (result / 10) % 10);
-        _putchar('0' + result % 10);
-    }
-}
-
-/**
  * print_times_table - prints the n times table, starting with 0.
  * @n: The number to print the times table for.
  *
@@ -54,7 +20,32 @@ void print_times_table(int n)
         for (j = 0; j <= n; j++)
         {
             result = i * j;
-            print_single_number(result, j == 0);
+
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+
+            if (result < 10)
+            {
+                if (j != 0)
+                    _putchar(' ');
+                _putchar(' ');
+                _putchar('0' + result);
+            }
+            else if (result < 100)
+            {
+                _putchar(' ');
+                _putchar('0' + result / 10);
+                _putchar('0' + result % 10);
+            }
+            else
+            {
+                _putchar('0' + result / 100);
+                _putchar('0' + (result / 10) % 10);
+                _putchar('0' + result % 10);
+            }
         }
         _putchar('\n');
     }
