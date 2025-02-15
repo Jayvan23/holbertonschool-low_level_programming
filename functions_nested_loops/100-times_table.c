5d18aa79c5ba556934a27ea8d413e23eaf896b37
Jayvan <jayvanramos23@gmail.com>
2025-02-15 17:13:39 -0400

    fixing codes

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -20,33 +20,24 @@ void print_times_table(int n)
         {
             result = i * j;
 
-            if (j == 0)
-                _putchar('0');
-            else
+            /* Print comma and spacing for all columns except the first */
+            if (j != 0)
             {
                 _putchar(',');
                 _putchar(' ');
 
+                /* Add spacing for alignment */
                 if (result < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
+                    _putchar(' '), _putchar(' ');
                 else if (result < 100)
-                {
                     _putchar(' ');
-                }
             }
 
+            /* Print the result */
             if (result >= 100)
-            {
-                _putchar((result / 100) + '0');
-                _putchar(((result / 10) % 10) + '0');
-            }
+                _putchar((result / 100) + '0'), _putchar(((result / 10) % 10) + '0');
             else if (result >= 10)
-            {
                 _putchar((result / 10) + '0');
-            }
             _putchar((result % 10) + '0');
         }
         _putchar('\n');
