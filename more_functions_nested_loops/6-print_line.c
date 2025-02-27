881fc214181eaec6e0cb2682d95003b9aa92d86c
Jayvan <jayvanramos23@gmail.com>
2025-02-27 14:30:18 -0400

    added 0 to 10

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,15 +6,15 @@
  */
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < n; i++)
-        _putchar('_');
-    _putchar('\n');
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
 }
