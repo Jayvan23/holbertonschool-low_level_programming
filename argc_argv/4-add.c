0fadbcd15c5adf1ea8db7f63ea63b174806920bf
Jayvan <jayvanramos23@gmail.com>
2025-03-05 11:26:50 -0400

    added 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_number - Checks if a string consists only of digits
+ * @s: The string to check
+ *
+ * Return: 1 if a number, 0 otherwise
+ */
+int is_number(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+		return (0);
+
+	while (s[i])
+	{
+		if (!isdigit(s[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - Adds positive numbers
+ * @argc: Argument count
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: 0 if successful, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIn_H
+
+int is_number(char *s)
+int main(int argc, char *argv[])
+
+#endif
