2a1f83ca1f6f5e2ce5d5fd7ab3fd1f2a036cd27b
Jayvan <jayvanramos23@gmail.com>
2025-03-05 11:20:10 -0400

    added 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Prints all arguments received
+ * main - Multiplies two numbers
  * @argc: Argument count
  * @argv: Argument vector (array of strings)
  *
- * Return: Always 0
+ * Return: 0 if successful, 1 if error
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
+		printf("Error\n");
+		return (1);
 	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
