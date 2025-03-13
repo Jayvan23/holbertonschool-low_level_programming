c311f314290835f147adc97bff3d08023bbaab6e
Jayvan <jayvanramos23@gmail.com>
2025-03-13 14:33:46 -0400

    added 0 to 5

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
-/* Define a new type struct dog */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - structure for storing dog information
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
-/* Define a new type dog_t as a new name for the type struct dog */
 typedef struct dog dog_t;
 
-/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
