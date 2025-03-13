e65f64f75ad8314246ab0b8a7dc8df2c4be7ec64
Jayvan <jayvanramos23@gmail.com>
2025-03-13 14:28:48 -0400

    added 0 to 5

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include "dog.h"
 
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
