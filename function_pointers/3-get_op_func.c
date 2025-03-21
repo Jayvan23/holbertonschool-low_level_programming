39f54420bf528989b7c5ba95831e269b172e5dd9
Jayvan <jayvanramos23@gmail.com>
2025-03-21 12:26:32 -0400

    added 0 to 2

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stddef.h>
+#include <string.h>
 
 /**
  * get_op_func - Selects the correct function for the operation
@@ -21,7 +22,8 @@ int (*get_op_func(char *s))(int, int)
 
     while (ops[i].op)
     {
-        if (*s == *(ops[i].op))
+        /* Compare full operator string instead of just first character */
+        if (strcmp(s, ops[i].op) == 0)
             return (ops[i].f);
         i++;
     }
