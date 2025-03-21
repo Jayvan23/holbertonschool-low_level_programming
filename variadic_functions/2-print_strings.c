cdf33d0499a3706651b7388417d6721ab60cc330
Jayvan <jayvanramos23@gmail.com>
2025-03-21 12:57:59 -0400

    added 0 to 3

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -8,22 +8,22 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    char *str;
+	va_list args;
+	unsigned int i;
+	char *str;
 
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-    {
-        str = va_arg(args, char *);
-        if (str)
-            printf("%s", str);
-        else
-            printf("(nil)");
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
 
-        if (separator && i < n - 1)
-            printf("%s", separator);
-    }
-    va_end(args);
-    printf("\n");
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
 }
