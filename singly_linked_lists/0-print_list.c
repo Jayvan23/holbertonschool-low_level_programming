6d6d8a425a9795cccac782b46ea4db2698529e62
Jayvan <jayvanramos23@gmail.com>
2025-04-07 12:24:40 -0400

    added 0 to 4

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the list
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h)
+    {
+        if (h->str)
+            printf("[%u] %s\n", h->len, h->str);
+        else
+            printf("[0] (nil)\n");
+        h = h->next;
+        count++;
+    }
+
+    return (count);
+}
---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to add
+ *
+ * Return: Address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new_node, *temp;
+    char *dup_str;
+
+    new_node = malloc(sizeof(list_t));
+    if (!new_node)
+        return (NULL);
+
+    dup_str = strdup(str);
+    if (!dup_str)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->str = dup_str;
+    new_node->len = strlen(dup_str);
+    new_node->next = NULL;
+
+    if (!*head)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+
+    temp = *head;
+    while (temp->next)
+        temp = temp->next;
+
+    temp->next = new_node;
+    return (new_node);
+}
